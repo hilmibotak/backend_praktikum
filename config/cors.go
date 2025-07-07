54122c09b2941986da9d6d6b10f77594eb720de3
hilmibotak <hilmiromadoni@gmail.com>
2025-07-07 14:27:38 +0700

    manambhkan di file cors

--- config/cors.go
+++ config/cors.go
@@ -3,9 +3,10 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:3000",
 	"http://localhost:5173/#",
-	"http://127.0.0.1:8088/",	
+	"http://127.0.0.1:8088/",
+	"https://frontend-praktikum-six.vercel.app/",
 }
 
-func GetAllowedOrigins() []string{
+func GetAllowedOrigins() []string {
 	return allowedOrigins
 }
